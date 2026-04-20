cc7736d8fd82cb06c6876648e9e34dc39b68ef53
Anthony <ajohnson21@ufl.edu>
2026-04-20 12:29:18 -0400

    useauth becoming usable for other functions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import { Inter } from 'next/font/google'
+import { AuthProvider } from '@/app/auth/auth'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
     </html>
   )
 }
